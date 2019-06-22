6ce2b644f7df4fe070d472012b5e796df026099d
Alexey V. Litvinov <lialsoftlab@yandex.ru>
2019-06-22 23:58:04 +1000

    Added flips table

---
+++ deploy/flips.sql
@@ -0,0 +1,17 @@
+-- Deploy flipr:flips to pg
+-- requires: users
+-- requires: appschema
+
+BEGIN;
+
+SET client_min_messages = 'warning';
+
+CREATE TABLE flipr.flips (
+    flip_id   BIGSERIAL   PRIMARY KEY,
+    nickname  TEXT        NOT NULL REFERENCES flipr.users(nickname),
+    body      TEXT        NOT NULL DEFAULT '' CHECK ( length(body) <= 180 ),
+    timestamp TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp()
+);
+
+COMMIT;
+
---
+++ verify/flips.sql
@@ -0,0 +1,15 @@
+-- Verify flipr:flips on pg
+
+BEGIN;
+
+SELECT flip_id
+     , nickname
+     , body
+     , timestamp
+FROM 
+    flipr.flips
+WHERE 
+    FALSE;
+
+
+ROLLBACK;
